8079de444609b1f884a7488469ae180ffd7ae3b5
John Galt <johngaltfirstrun@gmail.com>
2023-02-22 12:59:43 -0500

    peridot: Utilize task_profiles.json + cgroups.json
    We have full kernel support for uclamp and all additional functionality
    provided on current profiles.
    
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -577,6 +577,11 @@ PRODUCT_PACKAGES += \
     libhidlbase_shim \
     libinput_shim
 
+# Task Profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
+
 # Telephony
 PRODUCT_PACKAGES += \
     QtiTelephonyCompat
