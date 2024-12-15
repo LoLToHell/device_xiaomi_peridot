4a6e6570a49633dd551b182f1d1814c96f1884f4
Guidix <guidixm@gmail.com>
2024-12-15 03:05:35 +0200

    peridot: Align items

--- device.mk
+++ device.mk
@@ -512,7 +512,8 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.qti \
-    android.hardware.usb.gadget-service.qti
+    android.hardware.usb.gadget-service.qti \
+    libusbhost.vendor
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
@@ -526,7 +527,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
-    init.qcom.usb.sh
+    init.qcom.usb.sh \
+    usb_compositions.conf
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
